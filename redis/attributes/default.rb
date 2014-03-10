783acd976a5ba0986fb35eb70f96c15a7a196d19
namikawa <rx7.fd3s.rs@gmail.com>
2014-03-10 21:52:34 +0900

    add sentinel.conf

--- redis/attributes/default.rb
+++ redis/attributes/default.rb
@@ -1,2 +1,16 @@
 default['redis']['rpmfile'] = "redis-2.8.7-1.el6.x86_64.rpm"
 
+default['redis']['dir'] = "/var/lib/redis"
+
+default['redis-sentinel']['port']
+default['redis-sentinel']['logfile'] = "/var/log/redis/sentinel.log"
+default['redis-sentinel']['pidfile'] = "/var/run/redis/sentinel.pid"
+
+default['redis-sentinel']['master']['name'] = "mymaster"
+default['redis-sentinel']['master']['host'] = "127.0.0.1"
+default['redis-sentinel']['master']['port'] = "6379"
+default['redis-sentinel']['quorum'] = "2"
+default['redis-sentinel']['down-after-milliseconds'] = "10000"
+default['redis-sentinel']['failover-timeout'] = "600000"
+default['redis-sentinel']['parallel-syncs'] = "1"
+
