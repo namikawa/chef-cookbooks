492121c628d1b550ef1d63a0caccea36bd4527d1
namikawa <rx7.fd3s.rs@gmail.com>
2014-04-15 14:34:55 +0900

    support attribute for append only mode .

--- redis/attributes/default.rb
+++ redis/attributes/default.rb
@@ -1,6 +1,6 @@
 default['redis']['rpmfile'] = "redis-2.8.8-1.el6.x86_64.rpm"
 
-##### redis.conf #####
+########## redis.conf ##########
 default['redis']['pidfile'] = "/var/run/redis/redis.pid"
 default['redis']['port'] = "6379"
 default['redis']['tcp-backlog'] = "511"
@@ -15,6 +15,7 @@ default['redis']['syslog-ident'] = "redis"
 default['redis']['syslog-facility'] = "local0"
 default['redis']['databases'] = "16"
 
+### SNAPSHOTTING
 default['redis']['save'] = [
   "900 1",
   "300 10",
@@ -25,6 +26,8 @@ default['redis']['rdbcompression'] = "yes"
 default['redis']['rdbchecksum'] = "yes"
 default['redis']['dbfilename'] = "dump.rdb"
 default['redis']['dir'] = "/var/lib/redis"
+
+### REPLICATION
 
 # 認証を行う場合は、以下attributeの設定を行う
 # default['redis']['masterauth'] = "master-password"
@@ -38,10 +41,21 @@ default['redis']['repl-backlog-size'] = "50mb"
 
 default['redis']['slave-priority'] = "100"
 
+### LIMITS
 default['redis']['maxclients'] = "10000"
 
 # maxmemoryのattributeをnilとした場合、搭載メモリの70%を割り当てる設定を行う
 default['redis']['maxmemory'] = nil
+
+### APPEND ONLY MODE
+default['redis']['appendonly'] = "no"
+default['redis']['appendfilename'] = "appendonly.aof"
+default['redis']['appendfsync'] = "everysec"
+default['redis']['no-appendfsync-on-rewrite'] = "no"
+
+default['redis']['auto-aof-rewrite-percentage'] = "100"
+default['redis']['auto-aof-rewrite-min-size'] = "64mb"
+
 
 ##### redis-sentinel.conf #####
 default['redis-sentinel']['port'] = "26379"
