78bb782d19cb89c262f8a9835994f3c7e4a03bb9
namikawa <rx7.fd3s.rs@gmail.com>
2014-04-22 15:17:01 +0900

    add test

--- redis/spec/recipes/default_spec.rb
+++ redis/spec/recipes/default_spec.rb
@@ -1,7 +1,11 @@
 require_relative '../spec_helper'
 
 describe 'redis::default' do
-  let (:chef_run) { ChefSpec::Runner.new(platform:'centos', version:'6.4').converge(described_recipe) }
+  let (:chef_run) do
+    chef_run = ChefSpec::Runner.new(platform:'centos', version:'6.4') do |node|
+      node.set[:memory][:total] = "1024000"
+    end.converge(described_recipe)
+  end
 
   it "install redis" do
     %w{
@@ -14,6 +18,10 @@ describe 'redis::default' do
 
   it 'configure redis' do
     expect(chef_run).to render_file('/etc/redis.conf').with_content("save 300 10")
+  end
+
+  it 'configure redis maxmemory' do
+    expect(chef_run).to render_file('/etc/redis.conf').with_content("maxmemory 700mb")
   end
 
   it 'configure sentinel' do
