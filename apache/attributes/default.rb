08c33dd998db483f13e03015dd6ddc054fb30318
namikawa <rx7.fd3s.rs@gmail.com>
2014-10-20 16:52:02 +0900

    add attribute

--- apache/attributes/default.rb
+++ apache/attributes/default.rb
@@ -39,3 +39,10 @@ default['apache']['sslhost']['RequestHeader'] = [
 default['apache']['sslhost']['ProxyPass'] = "http://127.0.0.1:8000/ keepalive=On"
 default['apache']['sslhost']['ProxyPassReverse'] = "http://127.0.0.1:8000/"
 
+default['apache']['server-info']['RequireIP'] = [
+  "127.0.0.1/32",
+  "10.0.0.0/8",
+  "172.16.0.0/12",
+  "192.168.0.0/16",
+]
+
